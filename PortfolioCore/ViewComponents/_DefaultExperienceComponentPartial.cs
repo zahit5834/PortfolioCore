cd10a2463d489ce8c98c08fe68d5785aebf438bd
Mehmet <mehmetzahir5834@gmail.com>
2025-04-18 21:58:43 +0300

    updated project

---
+++ PortfolioCore/ViewComponents/_DefaultExperienceComponentPartial.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioCore.Context;
+
+namespace PortfolioCore.ViewComponents
+{
+    public class _DefaultExperienceComponentPartial : ViewComponent
+    {
+        PortfolioContext context = new PortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            var value = context.Experiences.ToList();
+            return View(value);
+        }
+    }
+}
+
---
+++ PortfolioCore/ViewComponents/_DefaultSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioCore.ViewComponents
+{
+    public class _DefaultSidebarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
